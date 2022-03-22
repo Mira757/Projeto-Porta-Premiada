3f36f2236d8e72f5877d8c9838d63a4ace14af3a
Mira757 <joao.veloz757@gmail.com>
2022-03-22 17:23:38 -0300

    Usando Router

---
+++ .history/pages/jogo/[portas]/[temPresente]_20220322171923.tsx
@@ -0,0 +1,30 @@
+import styles from "../../../styles/Jogo.module.css"
+import { useEffect, useState } from "react";
+import Porta from "../../../components/Porta";
+import { atualizarPortas, criarPortas } from "../../../functions/portas";
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+export default function jogo () {
+  const router = useRouter()
+  const [portas,setPortas] = useState(criarPortas(4,2))
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} onChange={novaporta => setPortas(atualizarPortas(portas, novaporta))}/>
+    })
+  }
+  
+  return (
+    <div id={styles.jogo}>
+      <div className={styles.portas}>
+        {renderizarPortas()}
+      </div>
+      <div className={styles.botoes}>
+        <Link href={"/"}>
+          <button>Reiniciar</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ .history/pages/jogo/[portas]/[temPresente]_20220322172141.tsx
@@ -0,0 +1,36 @@
+import styles from "../../../styles/Jogo.module.css"
+import { useEffect, useState } from "react";
+import Porta from "../../../components/Porta";
+import { atualizarPortas, criarPortas } from "../../../functions/portas";
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+export default function jogo () {
+  const router = useRouter()
+  const [portas,setPortas] = useState(criarPortas(4,2))
+
+  useEffect(() => {
+    const portas = +router.query.portas
+    const temPresente = +router.query.temPresente
+    setPortas(criarPortas(portas,temPresente))
+  }, [router?.query])
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} onChange={novaporta => setPortas(atualizarPortas(portas, novaporta))}/>
+    })
+  }
+  
+  return (
+    <div id={styles.jogo}>
+      <div className={styles.portas}>
+        {renderizarPortas()}
+      </div>
+      <div className={styles.botoes}>
+        <Link href={"/"}>
+          <button>Reiniciar</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
